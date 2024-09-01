0ec3ad6fb4c86ad7db0147b848783cddb4e9c8a6
coderman2 <okquaye@st.ug.edu.gh>
2024-09-01 22:19:41 +0000

    NLP integration

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import telebot
 
 from pdfminer.high_level import extract_pages, extract_text
 
+from transformers import pipeline
+
 from bot import API_KEY
 
 text = extract_text("ug_rules.pdf")
 print(text)
+
+QA_pipepline = pipeline("question-amswering")
 
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
@@ -13,4 +17,21 @@ bot = telebot.TeleBot(API_KEY, parse_mode=None)
 def welcome_message(message):
     bot.reply_to(message, "Hello, welcome to the student's query bot.")
 
+@bot.message_handler(func=lambda message: True)
+def query_response(message):
+  query = message.text
+
+  result = QA_pipepline(question=query, context=text)
+
+  response = result['answser']
+  certainty = result['score']
+
+  if certainty > 0.3:
+     answer = f"Answer: {response} \n Certainty:{round(certainty,2)} "
+
+  else:
+     answer = "Sorry! I could'nt find a relevant answer in the document "
+
+  bot.reply_to(message, answer)
+  
 bot.infinity_polling()
